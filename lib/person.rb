f339ff914c2096e22f9867f039f3b126288ca300
jjlippman <jjlippman@earthlink.net>
2016-01-08 09:42:21 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+
+  # def initialize(breed, name)
+  #   @breed=breed
+  #   @name=name
+    
+  # end
+
+  def job
+    @job
+
+  end
+
+  def job=(job)
+    @job=job
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+     @name=name
+  end
+
+end
